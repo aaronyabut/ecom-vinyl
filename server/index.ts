41508680dbd17dc8edda8b8a68d1f406cc2eee70
Aaron Yabut <aaronjiego@yahoo.com>
2025-03-04 20:07:06 -0800

    server is connected

---
+++ server/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response} from 'express';
+
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get("/test", async(req: Request, res: Response) => {
+  res.json({ message: 'Hello from Express!' });
+})
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`); // Fixed HTTPS to HTTP
+});
