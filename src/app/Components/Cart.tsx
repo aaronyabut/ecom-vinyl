0338b48f79be20aa2ec3531287658cf99dbf968e
Aaron Yabut <aaronjiego@yahoo.com>
2025-05-22 11:44:37 -0700

    Initial add to cart header added

--- src/app/Components/Cart.tsx
+++ src/app/Components/Cart.tsx
@@ -1,24 +1,58 @@
 import styles from './cart.module.scss';
+import XIcon from './../../../public/x-icon.svg';
+import Image from 'next/image';
 
 interface CartProps {
   setToCart: React.Dispatch<React.SetStateAction<boolean>>;
   toCart: boolean;
 }
+export const shoppingCart = [];
 
 export default function Cart ({
   setToCart,
   toCart
 } : CartProps) {
   return (
-<div className={`${styles.cartContainer} ${toCart ? styles.showCart : styles.hideCart}`}>
-  <div
-    className={`${styles.bg} ${toCart ? styles.visible : ''}`}
-    onClick={() => setToCart(false)}
-  >
-  </div>
-  <div className={`${styles.cart} ${toCart ? styles.visible : ''}`}>
-    Cart
-  </div>
-</div>
+    <div className={`${styles.cartContainer} ${toCart ? styles.showCart : styles.hideCart}`}>
+      <div
+        className={`${styles.bg} ${toCart ? styles.visible : ''}`}
+        onClick={() => setToCart(false)}
+      >
+      </div>
+      <div className={`${styles.cart} ${toCart ? styles.visible : ''}`}>
+        <div className={styles.topWrapper}>
+          <div className={styles.headerWrapper}>
+            <div className={styles.header}>
+              <div className={styles.title}>MY CART</div>
+              <div className={styles.icon}>
+                <Image
+                  src={XIcon}
+                  height={20}
+                  width={20}
+                  alt="x icon"
+                  onClick={() => setToCart(false)}
+                />
+              </div>
+            </div>
+            <div className={styles.totalProducts}>
+              {shoppingCart.length === 1 ? `${shoppingCart.length} Product` :
+              `${shoppingCart.length} Products`
+              }
+            </div>
+            <div className={styles.shippingWrapper}>
+              <div>PROGRESS BAR</div>
+              <div>CALC Shipping</div>
+            </div>
+            {/* <div className={styles.}></div> */}
+          </div>
+          <div className={styles.vinylsWrapper}>
+            BODY
+          </div>
+        </div>
+        <div className={styles.totalWrapper}>
+          FOOTER
+        </div>
+      </div>
+    </div>
   )
 }
